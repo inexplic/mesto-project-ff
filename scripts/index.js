9a06270cf7301d22bd435d194a05896b8905ee69
Diana Gajimagomedova <diana251199@mail.ru>
2024-07-21 15:01:13 +0500

    card deletion functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function deleteCard(event) {
+    const eventTarget = event.target;
+    eventTarget.closest('.card').remove();
+}
 
-// @todo: Функция создания карточки
+function addCard(cardName, cardImage, cardDelete) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    cardItem.querySelector('.card__title').textContent = cardName;
+    cardItem.querySelector('.card__image').src = cardImage;
 
-// @todo: Вывести карточки на страницу
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', cardDelete);
+
+    return cardItem;
+}
+
+initialCards.forEach(function (item) {
+    cardItem = addCard(item.name, item.link, deleteCard);
+
+    placesList.append(cardItem);
+})
